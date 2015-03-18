19fdfa0238367d6525afc4c6d4d9ad71c08b3db1
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-03-18 17:17:36 +0800

    Chapter 2 of GeoQuiz

---
+++ app/src/main/java/com/example/tommy/geoquiz/TrueFalse.java
@@ -0,0 +1,30 @@
+package com.example.tommy.geoquiz;
+
+/**
+ * Created by tommy on 3/18/2015.
+ */
+public class TrueFalse {
+    private int mQuestion;
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion) {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
