ef602d5f4a76db9f03331edc86b1ddeb5fb97981
tommy-cheng <tommy.cfc.cheng@gmail.com>
2015-03-27 14:18:57 +0800

    Chapter 6-0 getActionBar by modifying manifest and activity.xml which are not mention in the book - solution is in - http://stackoverflow.com/questions/10031180/getactionbar-returns-null

--- app/src/main/java/com/example/tommy/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/tommy/geoquiz/QuizActivity.java
@@ -1,8 +1,12 @@
 package com.example.tommy.geoquiz;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -63,11 +67,17 @@ public class QuizActivity extends Activity {
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
+    @TargetApi(11)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setSubtitle("Bodies of water");
+        }
 
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
 
